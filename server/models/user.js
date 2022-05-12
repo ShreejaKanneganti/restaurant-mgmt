f1839056b3d126e8d832a71c9b855e5eb6c39f7e
Shreejakanneganti <kshreeja19@gmail.com>
2022-05-12 11:01:25 -0400

    logging in

--- server/models/user.js
+++ server/models/user.js
@@ -9,4 +9,12 @@ const users = [
     }
    ];
    let getUsers = () => users;
+   function login(username, password) 
+   {
+    const user = await userExists(username);
+    if(!user[0]) throw Error('User not found')
+    if(user[0].user_password !== password) throw Error("Password is incorrect");
+  
+    return user[0];
+  }
    module.exports = { getUsers };
