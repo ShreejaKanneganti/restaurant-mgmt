c5897c51e38ada9f86d764cdd52f5cd90f03f0bd
Shreejakanneganti <kshreeja19@gmail.com>
2022-05-12 11:01:37 -0400

    logging in

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
  try {
- const users = User.getUsers();
+ const users =await User.getUsers();
  res.send(users);
  } catch(err) {
  res.status(401).send({message: err.message});
  }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+      const user = await User.login(req.body.username, req.body.password);
+      res.send({...user, password: undefined});
+    } catch (error) {
+      res.status(401).send({message: error.message});
+    }
+  })
 module.exports = router;
