345aa2965a9562e4f86f00e17fc9b03e22927672
Shreejakanneganti <kshreeja19@gmail.com>
2022-05-15 00:05:52 -0400

    add to cart

---
+++ public/javascripts/products.js
@@ -0,0 +1,57 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  1: {
+    name : "Paneer Tika",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "1.png",
+    price : 199
+  },
+  2: {
+    name : "Veg Pizza",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "2.png",
+    price : 149
+  },
+  3: {
+    name : "Paneer Pizza",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "3.png",
+    price : 129
+  },
+  4: {
+    name : "Cheese Chili Pizza",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "4.png",
+    price : 99
+  },
+  5: {
+    name : "Sandwich",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "5.png",
+    price : 49
+  },
+  6: {
+    name : "Burger",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "6.png",
+    price : 89
+  },
+  7: {
+    name : "French Fries",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "7.png",
+    price : 79
+  },
+  8: {
+    name : "Noodles",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "8.png",
+    price : 59
+  },
+  9: {
+    name : "Pasta",
+    desc : "Lorem ipsum dolor sit amet, consectetur elit.",
+    img : "9.png",
+    price : 69
+  }
+};
